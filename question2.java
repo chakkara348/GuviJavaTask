5c3a4048f06ae374b6f333435b7558b023d38ee4
chakkara348 <35254684+chakkara348@users.noreply.github.com>
2025-02-09 10:08:50 +0530

    Add files via upload

---
+++ question2.java
@@ -0,0 +1,18 @@
+package javaTask;
+
+public class question2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//declare the variable
+		int number = 3;
+		
+		if (number%2 == 0) {
+			System.out.println("The integer number " + number + " is even number");
+		}else {
+			System.out.println("The integer number " + number + " is odd number");
+		}
+	}
+
+}
